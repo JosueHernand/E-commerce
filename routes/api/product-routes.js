c2d31816f68963434dc28ecf62038be9857c8ba3
JosueHernand <joseh1337@gmail.com>
2023-11-07 05:51:16 -0800

    fixed some issue on both files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll ({
-      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tagged_products' }]
+      include: [{ model: Category }, { model: Tag, through: ProductTag }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk (req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tagged_products' }]
+      include: [{ model: Category }, { model: Tag, through: ProductTag }]
     });
 
     if (!productData) {
