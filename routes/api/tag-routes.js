621f0ec93a35845d6d483c7cf16f9405f1ae5da0
JosueHernand <joseh1337@gmail.com>
2023-11-07 03:57:12 -0800

    Added route to update a specific tag by ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,8 +39,20 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData[0]);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
