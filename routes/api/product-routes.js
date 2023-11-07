32b611da3413c540347ae8c33cb2ecd95673f41f
JosueHernand <joseh1337@gmail.com>
2023-11-07 04:00:24 -0800

    Added route to retrieve a list of products with associated categories and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll ({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tagged_products' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
